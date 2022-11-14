9b02d56fa5a43949d3177352a8f7b9658977b68e
bursh2021 <115107442+burshilya2023@users.noreply.github.com>
2022-11-14 18:11:30 +0300

    add 4

--- hooks/useAction.ts
+++ hooks/useAction.ts
@@ -1,11 +1,12 @@
 import {bindActionCreators} from '@reduxjs/toolkit'
 import {useDispatch} from 'react-redux'
+//@ts-ignore
 import { CartsActions } from '../slices/cartSlice/CartSlice'
 import { FiltersActions } from '../slices/filterSlice/filterSlice'
 import { FavoriteAction } from '../slices/favoriteSlice/favoriteSlice'
 import {ThemeAction} from '../slices/switchThemeSlice/themeSlice'
 import {setItems} from '../slices/pizzaSlice/pizzaSlice'
-
+ 
 const allActions={
     ...FiltersActions,
     ...CartsActions,
